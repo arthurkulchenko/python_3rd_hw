84784dba7be0a1a8be317a542c021c89084ac662
Artur <artur.cr@pm.me>
2018-09-14 15:43:36 +0200

    Update request_parser.py
    change tabs to spaces

--- lib/request_parser.py
+++ lib/request_parser.py
@@ -2,5 +2,5 @@ from mimetools import Message
 from StringIO import StringIO
 
 def parse(request):
-	request_line, headers_alone = request.split('\r\n', 1)
-	return Message(StringIO(headers_alone))
+    request_line, headers_alone = request.split('\r\n', 1)
+    return Message(StringIO(headers_alone))
